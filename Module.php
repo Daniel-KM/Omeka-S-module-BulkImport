b6d5ffb069a8070a558f2a3a01f2e21a3007c369
Daniel Berthereau <Daniel.github@Berthereau.net>
2019-01-28 00:00:00 +0100

    Fixed install of BulkImport.

--- Module.php
+++ Module.php
@@ -4,8 +4,14 @@ namespace BulkImport;
 require_once __DIR__ . '/src/Module/AbstractGenericModule.php';
 
 use BulkImport\Module\AbstractGenericModule;
+use Zend\ModuleManager\ModuleManager;
 
 class Module extends AbstractGenericModule
 {
     protected $dependency = 'Log';
+
+    public function init(ModuleManager $moduleManager)
+    {
+        require_once __DIR__ . '/vendor/autoload.php';
+    }
 }
