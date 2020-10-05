04a994969266c7113d228cd692aeb30b4cb3b8f7
Daniel Berthereau <Daniel.git@Berthereau.net>
2020-10-05 00:00:00 +0000

    Cleaned interfaces to return self.

--- src/Interfaces/Processor.php
+++ src/Interfaces/Processor.php
@@ -13,16 +13,19 @@ interface Processor
 
     /**
      * @param Reader $reader
+     * @return self
      */
     public function setReader(Reader $reader);
 
     /**
      * @param Logger $logger
+     * @return self
      */
     public function setLogger(Logger $logger);
 
     /**
      * @param Job $job
+     * @return self
      */
     public function setJob(Job $job);
 
