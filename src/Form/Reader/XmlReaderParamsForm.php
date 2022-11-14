d3aeb8a6d9fb2e0393dab84e431958e02cd57b49
Daniel Berthereau <Daniel.git@Berthereau.net>
2022-11-14 00:00:00 +0000

    Allowed to upload more xml formats.

--- src/Form/Reader/XmlReaderParamsForm.php
+++ src/Form/Reader/XmlReaderParamsForm.php
@@ -6,6 +6,48 @@ use Laminas\Form\Element;
 
 class XmlReaderParamsForm extends XmlReaderConfigForm
 {
+    /**
+     * @see \XmlViewer
+     *
+     * @var array
+     */
+    const MEDIATYPES_XML = [
+        'application/xml',
+        'text/xml',
+
+        'application/alto+xml',
+        'application/atom+xml',
+        'application/ead+xml', // Unofficial.
+        'application/marcxml+xml',
+        'application/mets+xml',
+        'application/mods+xml',
+        'application/rss+xml',
+        'application/tei+xml',
+        'application/xhtml+xml',
+
+        'application/vnd.alto+xml', // Deprecated in 2017.
+        'application/vnd.bnf.refnum+xml',
+        'application/vnd.ead+xml',
+        'application/vnd.iccu.mag+xml',
+        'application/vnd.marc21+xml', // Deprecated in 2011.
+        'application/vnd.mei+xml',
+        'application/vnd.mets+xml', // Deprecated in 2011.
+        'application/vnd.mods+xml', // Deprecated in 2011.
+        'application/vnd.openarchives.oai-pmh+xml',
+        'application/vnd.pdf2xml+xml', // Used in module IIIF Search.
+        'application/vnd.recordare.musicxml+xml',
+        'application/vnd.tei+xml', // Deprecated in 2011.
+
+        'application/vnd.oasis.opendocument.presentation-flat-xml',
+        'application/vnd.oasis.opendocument.spreadsheet-flat-xml',
+        'application/vnd.oasis.opendocument.text-flat-xml',
+        'application/vnd.openarchives.oai-pmh+xml',
+
+        'text/html',
+        'text/vnd.hocr+html', // Unofficial, not standard xml anyway. And recommended is alto.
+        'text/vnd.omeka+xml',
+    ];
+
     public function init(): void
     {
         // Set binary content encoding
@@ -21,7 +63,8 @@ class XmlReaderParamsForm extends XmlReaderConfigForm
                 'attributes' => [
                     'id' => 'file',
                     'required' => false,
-                    'accept' => 'text/xml,application/xml,xml',
+                    'accept' => implode(',', self::MEDIATYPES_XML)
+                        . ',alto,atom,ead,feed,fodp,fods,fodt,html,mag,mei,mets,mods,musicxml,mxl,pdf2xml,refnum,rss,tei,xhtml,xml',
                 ],
             ])
         ;
