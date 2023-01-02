9d1917f1d3fc188f96011d2456d2f4ac994ba087
Daniel Berthereau <Daniel.git@Berthereau.net>
2023-01-02 00:00:00 +0000

    Added a default importer for ead.

---
+++ data/importers/xml - ead.php
@@ -0,0 +1,31 @@
+<?php declare(strict_types=1);
+
+return [
+    'owner' => null,
+    'label' => 'Xml - Ead', // @translate
+    'config' => [],
+    'readerClass' => \BulkImport\Reader\XmlReader::class,
+    'readerConfig' => [
+        'xsl_sheet_pre' => '',
+        'xsl_sheet' => 'module:xsl/ead_to_resources.xsl',
+        'mapping_config' => 'module:xml/ead_to_omeka.xml',
+        'xsl_params' => [],
+    ],
+    'processorClass' => \BulkImport\Processor\ItemProcessor::class,
+    'processorConfig' => [
+        'skip_missing_files' => false,
+        'entries_to_skip' => 0,
+        'entries_max' => null,
+        'o:resource_template' => '',
+        'o:resource_class' => '',
+        'o:owner' => "current",
+        'o:is_public' => null,
+        'action' => 'create',
+        'action_unidentified' => 'skip',
+        'identifier_name' => [
+            'o:id',
+        ],
+        'allow_duplicate_identifiers' => false,
+        'resource_name' => '',
+    ],
+];
