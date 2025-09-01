e499a295ede23fba3182eb727cc801cdf5794f41
Daniel Berthereau <Daniel.git@Berthereau.net>
2025-09-01 00:00:00 +0000

    Preconfigured sru-srw on install.

---
+++ data/importers/xml - sru-srw.php
@@ -0,0 +1,33 @@
+<?php declare(strict_types=1);
+
+return [
+    'o:owner' => null,
+    'o:label' => 'Xml - SRU/SRW (OAI-DC)', // @translate
+    'o-bulk:reader' => \BulkImport\Reader\XmlReader::class,
+    'o-bulk:mapper' => null,
+    'o-bulk:processor' => \BulkImport\Processor\ItemProcessor::class,
+    'o:config' => [
+        'reader' => [
+            'xsl_sheet_pre' => '',
+            'xsl_sheet' => 'module:xsl/sru.dublin-core_to_omeka.xsl',
+            'xsl_params' => [],
+        ],
+        'mapper' => [
+        ],
+        'processor' => [
+            'entries_to_skip' => 0,
+            'entries_max' => null,
+            'o:resource_template' => '',
+            'o:resource_class' => '',
+            'o:owner' => "current",
+            'o:is_public' => null,
+            'action' => 'create',
+            'action_unidentified' => 'error',
+            'identifier_name' => [
+                'o:id',
+            ],
+            'allow_duplicate_identifiers' => false,
+            'resource_name' => '',
+        ],
+    ],
+];
